3af08b699920c399ae8218996a7b2f9c4e9a5c43
Pawan Jain <jainpawan211199@gmail.com>
2019-08-11 18:51:41 +0530

    Initial setup of signin and signUp page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" render={() => <SignIn />} exact />
+            <Route path="/signup" render={() => <SignUp />} exact />
+          </Switch>
+        </div>
+      </BrowserRouter>
+
     </div>
+
   );
 }
 
