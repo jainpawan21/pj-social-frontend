d246ff3d601b04e8e1170315c02996eff2575299
Pawan Jain <jainpawan211199@gmail.com>
2019-08-16 22:33:00 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import SignIn from './components/SignIn'
+// import SignUp from './components/SignUp'
+import Home from './components/Home'
+
 function App() {
+
+  // function requireCheck() {
+	// 	if (localStorage.getItem("token")) {
+	// 		return true;
+	// 	}
+	// 	return false;
+  // }
+  
+  
   return (
     <div>
       <BrowserRouter>
         <div>
           <Switch>
-            <Route path="/" render={() => <SignIn />} exact />
-            <Route path="/signup" render={() => <SignUp />} exact />
+            {/* <Route path="/" render={() => requireCheck() ? (<Redirect to ="/home" />) : (<SignIn />)} exact />
+            <Route path="/signup" render={() => requireCheck() ? (<Redirect to ="/home" />) : (<SignUp />)} exact /> */}
+            <Route path="/home" render={()=> <Home /> } />
           </Switch>
         </div>
       </BrowserRouter>
 
     </div>
-
-  );
+   );
 }
 
 export default App;
